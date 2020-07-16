5ed9c39eac8d86db7981b84d56ba5dd9f888335e
mckennaleo <mckenna.leo@gmail.com>
2020-07-16 15:59:33 -0400

    linting, commenting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,9 @@
-// Four steps in jQuery
-// Step one is Target
-// Step two is add event listener
-// Step Three (optional): Retarget/Self target
-// Effect
-
-$(document).ready(function() {
-    // --- our code goes here ---
-    // $('.title').click(function() {
-    //     $('.username h2').text('New Name');
-    //     $('.subtitle').text('Don\'t write a new tweet')
-    //     const h2 = $('.name').text()
-    //     console.log(h2)        
-    // });
-    $('.form-field').keyup(function() {
+//how many characters?
+$(document).ready(function () {
+    $('.form-field').keyup(function () {
         const textLength = 140 - $(this).val().length;
         $('.counter').val(textLength);
-        textLength <= 0  ? $('.counter').css('color', 'red') : $('.counter').css('color', '#545149');
+        textLength <= 0 ? $('.counter').css('color', 'red') : $('.counter').css('color', '#545149');
     })
-    
+
 });
